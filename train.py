922c7f3964f1aa968253562f17300f2507e3c89d
dyzy <33219672+kawhidiot@users.noreply.github.com>
2021-10-23 15:51:22 +0800

    fix multi gpu bug

--- train.py
+++ train.py
@@ -18,11 +18,6 @@ from tools.losses import get_loss
 import numpy as np
 from tools.utils import label_mapping
 from tools.parse_config_yaml import parse_yaml
-yaml_file = 'config.yaml'
-param_dict = parse_yaml(yaml_file)
-
-os.environ["CUDA_VISIBLE_DEVICES"] = param_dict['gpu_id']
-gpu_list = [i for i in range(len(param_dict['gpu_id'].split(',')))]
 
 
 def main():
@@ -46,8 +41,8 @@ def main():
 
     if param_dict['use_gpu']:
         print(gpu_list)
-        # model = torch.nn.DataParallel(frame_work, device_ids=gpu_list)  # use gpu to train
-        model = frame_work
+        model = torch.nn.DataParallel(frame_work, device_ids=gpu_list)  # use gpu to train
+        # model = frame_work
         # model = torch.nn.parallel.DistributedDataParallel(model, device_ids=gpu_list)
         model_id = 0
         if find_new_file(param_dict['model_dir']) is not None:
@@ -71,69 +66,67 @@ def main():
     optimizer = torch.optim.Adam(model.parameters(), lr=param_dict['base_lr'])  # define optimizer
     writer = SummaryWriter(os.path.join(param_dict['save_dir_model'], 'runs'))
 
-    f = open(os.path.join(param_dict['save_dir_model'], 'log.txt'), 'w')
-    for epoch in range(param_dict['epoches']):
-        model.train()
-        running_loss = 0.0
-        lr = adjust_learning_rate(param_dict['base_lr'], optimizer, epoch, model_id, param_dict['power'])  # adjust learning rate
-        batch_num = 0
-        for i, data in tqdm.tqdm(enumerate(trainloader)):  # get data
-            images, labels = data['image'], data['gt']
-            i += images.size()[0]
-            labels = labels.view(images.size()[0], param_dict['img_size'], param_dict['img_size']).long()
-            if param_dict['use_gpu']:
-                images = images.cuda()
-                labels = labels.cuda()
-            optimizer.zero_grad()
-            if param_dict['model_name'] != 'pspnet':
-                outputs = model(images)  # get prediction
-            else:
-                outputs, _ = model(images)
-            losses = criterion(outputs, labels)  # calculate loss
-            losses.backward()  #
-            optimizer.step()
-            running_loss += losses
-            if i % 50 == 0:
-                grid_image = make_grid(images[:3].clone().cpu().data, 3, normalize=True)
-                writer.add_image('image', grid_image)
-                grid_image = make_grid(
-                    tools.utils.decode_seg_map_sequence(torch.max(outputs[:3], 1)[1].detach().cpu().numpy()),
-                    3,
-                    normalize=False,
-                    range=(0, 255))
-                writer.add_image('Predicted label', grid_image)
-                grid_image = make_grid(
-                    tools.utils.decode_seg_map_sequence(torch.squeeze(labels[:3], 1).detach().cpu().numpy()),
-                    3,
-                    normalize=False, range=(0, 255))
-                writer.add_image('Groundtruth label', grid_image)
-            batch_num += images.size()[0]
-            # break
-        print('epoch is {}, train loss is {}'.format(epoch, running_loss.item() / batch_num))
-        writer.add_scalar('learning_rate', lr, epoch)
-        writer.add_scalar('train_loss', running_loss / batch_num, epoch)
+    with open(os.path.join(param_dict['save_dir_model'], 'log.txt'), 'w') as ff:
+        for epoch in range(param_dict['epoches']):
+            model.train()
+            running_loss = 0.0
+            lr = adjust_learning_rate(param_dict['base_lr'], optimizer, epoch, model_id, param_dict['power'])  # adjust learning rate
+            batch_num = 0
+            for i, data in tqdm.tqdm(enumerate(trainloader)):  # get data
+                images, labels = data['image'], data['gt']
+                i += images.size()[0]
+                labels = labels.view(images.size()[0], param_dict['img_size'], param_dict['img_size']).long()
+                if param_dict['use_gpu']:
+                    images = images.cuda()
+                    labels = labels.cuda()
+                optimizer.zero_grad()
+                if param_dict['model_name'] != 'pspnet':
+                    outputs = model(images)  # get prediction
+                else:
+                    outputs, _ = model(images)
+                losses = criterion(outputs, labels)  # calculate loss
+                losses.backward()  #
+                optimizer.step()
+                running_loss += losses
+                if i % 50 == 0:
+                    grid_image = make_grid(images[:3].clone().cpu().data, 3, normalize=True)
+                    writer.add_image('image', grid_image)
+                    grid_image = make_grid(
+                        tools.utils.decode_seg_map_sequence(torch.max(outputs[:3], 1)[1].detach().cpu().numpy()),
+                        3,
+                        normalize=False,
+                        range=(0, 255))
+                    writer.add_image('Predicted label', grid_image)
+                    grid_image = make_grid(
+                        tools.utils.decode_seg_map_sequence(torch.squeeze(labels[:3], 1).detach().cpu().numpy()),
+                        3,
+                        normalize=False, range=(0, 255))
+                    writer.add_image('Groundtruth label', grid_image)
+                batch_num += images.size()[0]
+                # break
+            print('epoch is {}, train loss is {}'.format(epoch, running_loss.item() / batch_num))
+            writer.add_scalar('learning_rate', lr, epoch)
+            writer.add_scalar('train_loss', running_loss / batch_num, epoch)
 
-        if epoch % param_dict['save_iter'] == 0:
-            torch.save(model.state_dict(), os.path.join(param_dict['model_dir'], '%d.pth' % (model_id + epoch)))
-            val_miou, val_acc, val_f1, val_loss = eval(valloader, model, criterion, epoch)
-            val_miou_true, val_acc_true, val_f1_true = image_infer(model, epoch)
-            # val_miou_true, val_acc_true, val_f1_true = 0.0, 0.0, 0.0
-            writer.add_scalar('val_miou', val_miou, epoch)
-            writer.add_scalar('val_acc', val_acc, epoch)
-            writer.add_scalar('val_f1', val_f1, epoch)
-            writer.add_scalar('val_miou_true', val_miou_true, epoch)
-            writer.add_scalar('val_acc_true', val_acc_true, epoch)
-            writer.add_scalar('val_f1_true', val_f1_true, epoch)
-            writer.add_scalar('val_loss', val_loss, epoch)
-            cur_log = 'epoch:{}, learning_rate:{}, train_loss:{}, val_loss:{}, val_f1:{}, val_acc:{}, val_miou:{}, \
-             val_f1_true:{}, val_acc_true:{}, val_miou_true:{}\n'.format(
-                str(epoch), str(lr), str(running_loss.item() / batch_num), str(val_loss), str(val_f1), str(val_acc),
-                str(val_miou), str(val_f1_true), str(val_acc_true), str(val_miou_true)
-            )
-            # note_log += cur_log
-            print(cur_log)
-            # draw_pic(note_log)
-            f.writelines(str(cur_log))
+            if epoch % param_dict['save_iter'] == 0:
+                torch.save(model.state_dict(), os.path.join(param_dict['model_dir'], '%d.pth' % (model_id + epoch)))
+                val_miou, val_acc, val_f1, val_loss = eval(valloader, model, criterion, epoch)
+                val_miou_true, val_acc_true, val_f1_true = image_infer(model, epoch)
+                # val_miou_true, val_acc_true, val_f1_true = 0.0, 0.0, 0.0
+                writer.add_scalar('val_miou', val_miou, epoch)
+                writer.add_scalar('val_acc', val_acc, epoch)
+                writer.add_scalar('val_f1', val_f1, epoch)
+                writer.add_scalar('val_miou_true', val_miou_true, epoch)
+                writer.add_scalar('val_acc_true', val_acc_true, epoch)
+                writer.add_scalar('val_f1_true', val_f1_true, epoch)
+                writer.add_scalar('val_loss', val_loss, epoch)
+                cur_log = 'epoch:{}, learning_rate:{}, train_loss:{}, val_loss:{}, val_f1:{}, val_acc:{}, val_miou:{}, \
+                 val_f1_true:{}, val_acc_true:{}, val_miou_true:{}\n'.format(
+                    str(epoch), str(lr), str(running_loss.item() / batch_num), str(val_loss), str(val_f1), str(val_acc),
+                    str(val_miou), str(val_f1_true), str(val_acc_true), str(val_miou_true)
+                )
+                print(cur_log)
+                ff.writelines(str(cur_log))
 
 
 def eval(valloader, model, criterion, epoch):
@@ -220,6 +213,12 @@ def adjust_learning_rate(base_lr, optimizer, epoch, model_id, power):
 
 
 if __name__ == '__main__':
+    yaml_file = 'config.yaml'
+    param_dict = parse_yaml(yaml_file)
+    os.environ["CUDA_VISIBLE_DEVICES"] = param_dict['gpu_id']
+    gpu_list = [i for i in range(len(param_dict['gpu_id'].split(',')))]
+    gx = torch.cuda.device_count()
+
     frame_work = get_net(param_dict['model_name'], param_dict['input_bands'], param_dict['num_class'], param_dict['img_size'])
     if os.path.exists(param_dict['model_dir']) is False:
         os.mkdir(param_dict['model_dir'])
