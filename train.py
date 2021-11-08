be836add859c2d20f7ab5fd703e8c9fed3f71f58
dongsj <jkobekawh@gmail.com>
2021-11-08 12:30:22 +0800

    add model graph visual tool

--- train.py
+++ train.py
@@ -20,6 +20,9 @@ from tools.losses import get_loss
 import numpy as np
 from tools.utils import label_mapping
 from tools.parse_config_yaml import parse_yaml
+import torch.onnx
+
+import netron
 
 
 def main():
@@ -237,6 +240,13 @@ if __name__ == '__main__':
 
     frame_work = get_net(param_dict['model_name'], param_dict['input_bands'], param_dict['num_class'],
                          param_dict['img_size'])
+    if param_dict['vis_graph']:
+        sampledata = torch.rand((1, param_dict['input_bands'], param_dict['img_size'], param_dict['img_size']))
+        o = frame_work(sampledata)
+        onnx_path = os.path.join(param_dict['model_dir'], "model_vis.onnx")
+        torch.onnx.export(frame_work, sampledata, onnx_path, opset_version=11)
+        netron.start(onnx_path)
+
     if os.path.exists(param_dict['model_dir']) is False:
         os.mkdir(param_dict['model_dir'])
     main()
