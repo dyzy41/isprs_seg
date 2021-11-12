1ae1b3868107901b52c45f7c8f3db83c4ed35ae6
dongsj <jkobekawh@gmail.com>
2021-11-12 17:01:57 +0800

    add offline pretrained model path

--- networks/common_func/get_backbone.py
+++ networks/common_func/get_backbone.py
@@ -1,5 +1,18 @@
 import timm
+import torch
 
-def get_model(model_name, in_c=3, pretrained=False):
-    model = timm.create_model(model_name, pretrained=pretrained, features_only=True, in_chans=in_c)
-    return model
+
+def get_model(model_name, in_c=3, pretrained=True,
+              checkpoint_path=None):
+    if pretrained:
+        model = timm.create_model(model_name, pretrained=pretrained,
+                                  features_only=True, in_chans=in_c)
+        return model
+    if checkpoint_path is not None:
+        model = timm.create_model(model_name, pretrained=False,
+                                  features_only=True, in_chans=in_c)
+        state_dict = torch.load(checkpoint_path, map_location='cpu')
+        del state_dict['fc' + '.weight']
+        del state_dict['fc' + '.bias']
+        model.load_state_dict(state_dict, strict=False)
+        return model
